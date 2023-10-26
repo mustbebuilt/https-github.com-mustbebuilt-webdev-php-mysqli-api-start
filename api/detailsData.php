6fc71ec1651ca6a32b25c6b159b9bcae93f652d0
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-10-26 09:10:38 +0100

    Setup23

--- api/detailsData.php
+++ api/detailsData.php
@@ -1,15 +1,2 @@
 <?php
-require_once("../includes/config.php");
-// JSON http header
-header('Content-Type: application/json; charset=utf-8');
-// query to one films
-$getFilmID = $_GET[ 'filmID' ] ?? null;
-// query to get film by filmID
-$stmt = $mysqli->prepare( "SELECT * FROM Films WHERE filmID = ?" );
-$stmt->bind_param( 'i', $getFilmID );
-$stmt->execute();
-$result = $stmt->get_result();
-$obj = $result->fetch_object();
-$json = json_encode($obj);
-echo $json;
-?>
+// see Repo Wiki for instructions on how to build this API
